dc2d95d300be5651530259ac58b86ed099fcdb4b
Christopher Plonski <christopherplonski@gmail.com>
2024-10-22 15:28:33 -0500

    Fixed

--- TemplateActivity.cpp
+++ TemplateActivity.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 template<typename T>
 T max_two(T val1, T val2) {
 	return (val1 > val2) ? val1 : val2;
@@ -22,18 +20,18 @@ T max_two(T val1, T val2, T val3) {
 
 template<typename T>
 void swap(T &ptr1, T &ptr2) {
-	T ptr1Clone = new (ptr1);
+	T ptr1Clone = T(ptr1);
 
 	ptr1 = ptr2;
 
-	ptr2 = &ptr1Clone;
+	ptr2 = ptr1Clone;
 }
 
 int main() {
 	int a = 5;
 	int b = 7;
 	
-	swap(&a, &b);
+	swap(a, b);
 
-	cout << "A: " << a << " | B: " << b;
+	std::cout << "A: " << a << " | B: " << b;
 }
